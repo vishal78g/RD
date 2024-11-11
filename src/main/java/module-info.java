b88a6eebf89ec11e0356212d566d036fc25f7c24
Vishal <vishalghorpade78@gmail.com>
2024-11-11 23:54:11 +0530

    Hello

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module rd.rd {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens rd.rd to javafx.fxml;
+    exports rd.rd;
+}
